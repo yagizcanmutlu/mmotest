2f0d2f3d9ad9fb530be5f51b666330c1ff62ad30
yagizcanmutlu <yagizcanmutlu@gmail.com>
2025-11-01 00:54:22 +0300

    v1.0

---
+++ public/loaders.module.js
@@ -0,0 +1,6 @@
+// public/loaders.module.js
+import { GLTFLoader }  from '/vendor/three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from '/vendor/three/examples/jsm/loaders/DRACOLoader.js';
+
+window.GLTFLoader  = GLTFLoader;
+window.DRACOLoader = DRACOLoader;
