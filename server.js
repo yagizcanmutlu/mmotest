a61061795ffb074e63a285a398aefe056d46041b
yagizcanmutlu <yagizcanmutlu@gmail.com>
2025-11-01 08:03:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-// server.js — Byzas Agora (Planets + Stylized Avatars)//
+// server.js — Byzas Agora (Planets + Stylized Avatars)
 const express = require("express");
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
 
-import path from "path";
-import express from "express";
 const app = express();
-
-app.use(express.static(path.join(process.cwd(), "public")));
-app.use("/models", express.static(path.join(process.cwd(), "public", "models"), {
-  maxAge: "30d", immutable: true
-}));
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*", methods: ["GET","POST"] }, // prototip: açık
